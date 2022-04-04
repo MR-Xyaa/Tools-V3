1adb0ee5c4ea4c3f1e3460596a4923397aea3ccb
Inu Ganteng <102247436+MR-Xyaa@users.noreply.github.com>
2022-04-04 14:17:27 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,28 @@
+## WARNA ASU #####
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+##################
+
+import random
+import sys
+import time
+import os
+def inu_ganteng_banget(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+user_reply = input("Namamu Bang? \n")
+inu-ganteng_banget(clear)
+
+
+
+
+
+print("Bang", user_reply)
